e2d1cd575c605d54bd4e52a05fb7e74dcdef24af
Webprojon <tokhirjonkhasanov@gmail.com>
2025-04-02 22:39:45 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,42 @@
-import Image from "next/image";
+import { MdOutlineContentCopy } from "react-icons/md";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	return (
+		<div className="flex flex-col gap-y-6 max-w-[950px] mt-10 px-4 mx-auto tracking-wider">
+			<div>
+				<h1 className="font-semibold text-[28px]">AI Blog Post Writer</h1>
+				<p className="font-light text-black/70">
+					Generate AI-powered blog posts in second
+				</p>
+			</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+			<input
+				type="text"
+				placeholder="Enter a topic or keywords"
+				className="border border-gray-300 rounded-md outline-none font-light py-3 md:py-2 px-3 text-black/70 placeholder:text-black/70"
+			/>
+
+			<button className="bg-blue-500 hover:bg-blue-600 transition-all py-3 md:py-2 rounded-md text-white cursor-pointer font-medium text-[18px]">
+				Generate
+			</button>
+
+			<div className="mx-auto border-gray-400 border border-t-transparent w-8 h-8 mt-3 rounded-full animate-spin"></div>
+
+			<div className="flex flex-col gap-y-2">
+				<div className="flex justify-between items-center">
+					<span className="font-medium text-lg">Generated Blog Post</span>
+					<MdOutlineContentCopy className="text-gray-500 cursor-pointer size-5" />
+				</div>
+
+				<div className="border border-gray-300 w-full rounded-md p-2">
+					<p className="font-light">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam
+						laboriosam earum non, optio vitae doloremque eaque sequi veritatis.
+						In maiores architecto eum ex quaerat ipsam ad impedit praesentium,
+						iusto facilis?
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
